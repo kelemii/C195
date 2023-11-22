4d6902851bf012a7cbbe03760d64a71a1e9f9acf
kelemii <98933496+kelemii@users.noreply.github.com>
2023-11-22 07:08:01 -0700

    added contact DAO

---
+++ src/DAO/ContactDAO.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import Model.Contact;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static Help.JDBC.connection;
+
+public class ContactDAO {
+    public static ObservableList<Contact> getAllContacts() throws SQLException {
+        ObservableList<Contact> contactObservableList = FXCollections.observableArrayList();
+        List<Contact> contactList = new ArrayList<>();
+        String sql = "SELECT * FROM client_schedule.CONTACTS";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()) {
+            int contactID = resultSet.getInt("Contact_ID");
+            String contactName = resultSet.getString("Contact_Name");
+            String contactEmail = resultSet.getString("Email");
+            Contact contact = new Contact(contactID, contactName, contactEmail);
+            contactList.add(contact);
+        }
+        contactList.forEach(contact -> contactObservableList.add(contact));
+        return contactObservableList;
+    }
+}
